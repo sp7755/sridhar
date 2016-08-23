952991d1049b02560a567bd9a873ee3c29200620
sridhar <sridhar.pattapalli@gmail.com>
2016-08-23 14:19:58 -0700

    crome is added

---
+++ crome/src/main/java/google/crome/app/org/App.java
@@ -0,0 +1,11 @@
+package google.crome.app.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
